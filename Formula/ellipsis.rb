c39921ad1fc92b08628478c473770eef8c9da4b1
Ben Darlow <github@kapowaz.net>
2026-04-15 17:55:30 +0100

    update ellipsis sha256 for shellcheck fix

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -2,7 +2,7 @@ class Ellipsis < Formula
   desc "Automated dotfile management and syncing on top of yadm"
   homepage "https://github.com/kapowaz/ellipsis"
   url "https://github.com/kapowaz/ellipsis/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "cc61f6195a01988b153837ad5f94acc2fe3ba6861069a7c9a0efd01db39a8f80"
+  sha256 "c550dd156c077a5be1439c0576388dbeb6b99cf61587eb1f09a286e91dc257a3"
   license "MIT"
 
   depends_on "yadm"
