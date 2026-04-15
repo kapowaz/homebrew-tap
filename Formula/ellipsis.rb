7a61f6e6caa340fe318da36d71f508af0804bd99
Ben Darlow <github@kapowaz.net>
2026-04-15 17:42:01 +0100

    update ellipsis sha256 for bash 3.2 compat fix

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -2,7 +2,7 @@ class Ellipsis < Formula
   desc "Automated dotfile management and syncing on top of yadm"
   homepage "https://github.com/kapowaz/ellipsis"
   url "https://github.com/kapowaz/ellipsis/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d2c6e2daca76fe2261206f05e7f278e776dc1c81a8a91bac46707a325d20c232"
+  sha256 "cc61f6195a01988b153837ad5f94acc2fe3ba6861069a7c9a0efd01db39a8f80"
   license "MIT"
 
   depends_on "yadm"
