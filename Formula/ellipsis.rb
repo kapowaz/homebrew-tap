dc7781d369051b24c7d499c772256dee5b49bf65
Ben Darlow <github@kapowaz.net>
2026-04-15 19:36:42 +0100

    update ellipsis sha256 for grep regex fix

--- Formula/ellipsis.rb
+++ Formula/ellipsis.rb
@@ -2,7 +2,7 @@ class Ellipsis < Formula
   desc "Automated dotfile management and syncing on top of yadm"
   homepage "https://github.com/kapowaz/ellipsis"
   url "https://github.com/kapowaz/ellipsis/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "c550dd156c077a5be1439c0576388dbeb6b99cf61587eb1f09a286e91dc257a3"
+  sha256 "495808acf3a65805175998b773986873b661abe658b86126c7ee24036b1f6ffd"
   license "MIT"
 
   depends_on "yadm"
